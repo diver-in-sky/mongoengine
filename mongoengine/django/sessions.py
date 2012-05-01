52b9caaa4cdc9e6011789c98ca9091f04a3c4cfe
Andrew Tereskin <tereskin@gmail.com>
2012-05-01 16:12:24 +0400

    fix for django 1.3

--- mongoengine/django/sessions.py
+++ mongoengine/django/sessions.py
@@ -51,7 +51,9 @@ class SessionStore(SessionBase):
             return
 
     def save(self, must_create=False):
-        s = MongoSession(session_key=self._get_or_create_session_key())
+        if self.session_key is None:
+            self._session_key = self._get_new_session_key()
+        s = MongoSession(session_key=self.session_key)
         s.session_data = self.encode(self._get_session(no_load=must_create))
         s.expire_date = self.get_expiry_date()
         try:
